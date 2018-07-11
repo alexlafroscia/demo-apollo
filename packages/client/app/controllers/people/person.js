dc6f96e94ccd53c6f0a247bf7748f9e094207934
Alex LaFroscia <alex@lafroscia.com>
2018-07-11 11:00:24 -0700

    feat: support renaming people

--- packages/client/app/controllers/people/person.js
+++ packages/client/app/controllers/people/person.js
@@ -1,6 +1,29 @@
 import Controller from "@ember/controller";
-import { alias } from "@ember/object/computed";
+import { inject as service } from "@ember/service";
+import { alias, oneWay } from "@ember/object/computed";
+import mutation from "demo-apollo-client/gql/mutations/update-person";
 
 export default Controller.extend({
-  person: alias("model")
+  apollo: service(),
+
+  person: alias("model"),
+  bufferedName: oneWay("person.name"),
+
+  actions: {
+    /**
+     * Note: No need to update the `model` on the controller! The watched query
+     * in the route will automatically update with the new state, pushed from the
+     * server
+     */
+    async updateName({ target: { value } }) {
+      let variables = {
+        id: this.person.id,
+        input: {
+          name: value
+        }
+      };
+
+      await this.apollo.mutate({ mutation, variables }, "updatePerson");
+    }
+  }
 });
